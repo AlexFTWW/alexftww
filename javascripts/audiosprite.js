b7d30f1dbac719b13d79d8e89c2f3736655a0613
Alex Alvarez <alexftww@yahoo.com>
2014-06-30 02:25:50 +0800

    Update audiosprite.js

--- javascripts/audiosprite.js
+++ javascripts/audiosprite.js
@@ -1,14 +1,9 @@
-window.addEventListener('touchstart', function() {
-
-	// create empty buffer
-	var buffer = myContext.createBuffer(1, 1, 22050);
-	var source = myContext.createBufferSource();
-	source.buffer = buffer;
-
-	// connect to output (your speakers)
-	source.connect(myContext.destination);
-
-	// play the file
-	source.noteOn(0);
-
-}, false);
+function play() {
+    var audio = document.getElementById('audio1');
+    if (audio.paused) {
+        audio.play();
+    }else{
+        audio.pause();
+        audio.currentTime = 0
+    }
+}
